64e108858fd1120e3f4f3f52e6ceb66d5fdf315b
angel <angelnolasco24@gmail.com>
2023-10-09 15:42:08 -0400

    email

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,7 +6,7 @@ const Resume = () => {
     <div className="resume-container">
       <h1 className="resume-title">Resume</h1>
       <p className="resume-text">Click the button below to veiw my resume:</p>
-      <a href="https://docs.google.com/document/d/183VracxWVLWlBCPTd1J1hrOmPzOOwWjR2Vjx1ADjqSw/edit#" className="resume-download" download>
+      <a href="https://docs.google.com/document/d/12QlRHZpw_l-mtlq-Vk9wluUIhZG0YYmeI2icHR8tTUQ/edit?usp=sharing" className="resume-download" download>
         Veiw Resume
       </a>
     </div>
