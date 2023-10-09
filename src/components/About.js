1388ec9ad0871b0683d78c501895ed0af9822187
angel <angelnolasco24@gmail.com>
2023-10-09 15:03:29 -0400

    testing

--- src/components/About.js
+++ src/components/About.js
@@ -7,12 +7,9 @@ const About = () => {
       <h1 className="about-title">About Me</h1>
       <div className="about-content">
         <p>
-        As a current student in a software engineering bootcamp, I bring a fresh perspective and a passion for innovation to my work as a software engineer. With a background in customer service, I am a collaborative and dedicated team player who believes in the power of effective communication and teamwork to deliver successful software projects. I am proficient in various programming languages and web development, continuously expanding my skills and knowledge in these areas.
+        Dedicated full-stack software engineer with a foundation in the hospitality sector, delivering dedicated solutions. I bring a unique set of skills and experiences to tech, focusing on creating accessible, user-friendly solutions. My ability to simplify complex problems and collaborate effectively, honed through my restaurant background, complements my design sensibility. I'm enthusiastic about innovative, impactful web development projects.
 
-        In addition to my technical expertise, I am an active individual outside of work. I am an avid practitioner of Jiu-Jitsu, which not only keeps me physically fit but also teaches me discipline, resilience, and problem-solving skills. I find joy in the challenges and camaraderie that come with this martial art. Exploring the great outdoors through hiking is another passion of mine, as it allows me to disconnect and find inspiration in nature.
 
-        Lastly, I am a man of faith and strive to live a life of integrity and purpose. Being a stand-up individual is important to me, both personally and professionally. If you're looking for a motivated and adaptable software engineer who brings a diverse set of skills, a commitment to personal growth, and a strong sense of discipline and teamwork, I would be delighted to connect with you and discuss potential opportunities. Let's collaborate and create innovative solutions together.
-   
         </p>
       </div>
     </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
       <div className="home-container">
         <h1 className="home-title">I'm Angel A Nolasco</h1>
-        <p className="home-description">a Jr Software Engineer</p>
+        <p className="home-description">A Software Engineer</p>
       </div>
   );
 };
