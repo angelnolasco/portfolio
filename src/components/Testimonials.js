782229d6804505430c9a4533b7a5a21708bbfd62
angel <angelnolasco24@gmail.com>
2023-10-09 16:05:37 -0400

    testing 4

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -5,20 +5,20 @@ import "./Testimonials.css";
 const Testimonials = () => {
   const testimonials = [
     {
-      name: 'John Doe',
-      company: 'ABC Company',
-      testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat tristique volutpat.'
+      // name: 'John Doe',
+      // company: 'ABC Company',
+      // testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat tristique volutpat.'
     },
     {
-      name: 'Jane Smith',
-      company: 'XYZ Corporation',
-      testimonial: 'Nulla vitae nisl id quam vulputate cursus. Sed non purus rutrum, ullamcorper tortor ac, aliquet ipsum.'
+      // name: 'Jane Smith',
+      // company: 'XYZ Corporation',
+      // testimonial: 'Nulla vitae nisl id quam vulputate cursus. Sed non purus rutrum, ullamcorper tortor ac, aliquet ipsum.'
     },
   ];
 
   return (
     <div className="testimonials-container">
-      <h1 className="testimonials-title">Testimonials</h1>
+      <h1 className="testimonials-title"></h1>
       {testimonials.map((testimonial, index) => (
         <div className="testimonial" key={index}>
           <h2>{testimonial.name}</h2>
