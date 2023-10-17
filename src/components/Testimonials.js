f3e0d63ef69914cb1ef4dbddd419f6b81922af7d
angel <angelnolasco24@gmail.com>
2023-10-17 14:43:23 -0400

    testing 9

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -2,32 +2,32 @@ import React from 'react';
 import "./Testimonials.css";
 
 
-// const Testimonials = () => {
-//   const testimonials = [
-//     {
-//       // name: 'John Doe',
-//       // company: 'ABC Company',
-//       // testimonial: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed volutpat tristique volutpat.'
-//     },
-//     {
-//       // name: 'Jane Smith',
-//       // company: 'XYZ Corporation',
-//       // testimonial: 'Nulla vitae nisl id quam vulputate cursus. Sed non purus rutrum, ullamcorper tortor ac, aliquet ipsum.'
-//     },
-//   ];
+const Testimonials = () => {
+  const testimonials = [
+    {
+      name: 'Josue Romero',
+      company: 'nonprofit organizations',
+      testimonial: " Angel's expertise shone through as he swiftly addressed and resolved multiple bugs on my website. His coding proficiency is truly impressive, and his solutions were both effective and efficient"
+    },
+    {
+      name: ' Gillian Fitzgerald',
+      company: 'general assembly student ',
+      testimonial: "Collaborating with Angel on a Django web application was a valuable experience. He provided excellent support, actively listened, and offered valuable feedback throughout our teamwork."
+    },
+  ];
 
-//   return (
-//     <div className="testimonials-container">
-//       <h1 className="testimonials-title"></h1>
-//       {testimonials.map((testimonial, index) => (
-//         <div className="testimonial" key={index}>
-//           <h2>{testimonial.name}</h2>
-//           <p>{testimonial.company}</p>
-//           <blockquote>{testimonial.testimonial}</blockquote>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+  return (
+    <div className="testimonials-container">
+      <h1 className="testimonials-title"></h1>
+      {testimonials.map((testimonial, index) => (
+        <div className="testimonial" key={index}>
+          <h2>{testimonial.name}</h2>
+          <p>{testimonial.company}</p>
+          <blockquote>{testimonial.testimonial}</blockquote>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Testimonials;
