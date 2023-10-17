314aadae6486741e7273dd468c75a5c7b2c73e9e
angel <angelnolasco24@gmail.com>
2023-10-17 11:05:02 -0400

    testing 7

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import "./Testimonials.css";
+// import React from 'react';
+// import "./Testimonials.css";
 
 
 // const Testimonials = () => {
@@ -30,4 +30,4 @@ import "./Testimonials.css";
 //   );
 // };
 
-export default Testimonials;
+// export default Testimonials;
