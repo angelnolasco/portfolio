e639e104ad6de220814c8e23412b1d6abe4a0899
angel <angelnolasco24@gmail.com>
2023-10-24 23:35:10 -0400

    resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -5,9 +5,9 @@ const Resume = () => {
   return (
     <div className="resume-container">
       <h1 className="resume-title">Resume</h1>
-      <p className="resume-text">Click the button below to veiw my resume:</p>
+      <p className="resume-text">Click the button below to view my resume:</p>
       <a href="https://docs.google.com/document/d/12QlRHZpw_l-mtlq-Vk9wluUIhZG0YYmeI2icHR8tTUQ/edit?usp=sharing" className="resume-download" download>
-        Veiw Resume
+        View Resume
       </a>
     </div>
   );
