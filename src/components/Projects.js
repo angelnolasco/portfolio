c4c7095e37ed14132925ff0587dea414e1076b59
angel <angelnolasco24@gmail.com>
2023-10-25 21:27:41 -0400

    project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,23 +6,27 @@ const Projects = () => {
     {
       title: 'Musician Discovery Tool',
       description: "Built with HTML, CSS & JavaScript (jQuery)",
-      link: 'https://angelnolasco.github.io/project-1/'
+      deployLink: 'https://angelnolasco.github.io/project-1/',
+      repoLink: 'https://github.com/angelnolasco/project-1',
     },
     {
       title: 'FIGHT CLUB',
       description: "Built with EJS, CSS, JavaScript, MongoDB & Mongoose.",
-      link: 'https://angel-project-2.herokuapp.com/'
+      deployLink: 'https://angel-project-2.herokuapp.com/',
+      repoLink: 'https://github.com/angelnolasco/project-2',
     },
     {
       title: 'HIKEHUB',
       description: "Built with HTML, MongoDB, Express.js, React, Node.js & CSS.",
-      link:'https://timely-conkies-6ffe8a.netlify.app/'
+      deployLink: 'https://timely-conkies-6ffe8a.netlify.app/',
+      repoLink: 'https://github.com/angelnolasco/project-4-frontend',
     },
     {
       title: 'X-LIBRARY',
       description: "Built with Python-based Django Web Framework in collaboration with a team of talented and dedicated individuals.",
-      link: 'https://github.com/angelnolasco/x-library'
-    }
+      deployLink: 'https://x-library.herokuapp.com/',  
+      repoLink: 'https://github.com/angelnolasco/x-library',
+    },
   ];
 
   return (
@@ -32,8 +36,13 @@ const Projects = () => {
         <div key={index} className="project">
           <h2 className="project-title">{project.title}</h2>
           <p className="project-description">{project.description}</p>
-          <a href={project.link} className="project-link" target="_blank" rel="noopener noreferrer">
-            {project.link}
+          {project.deployLink && (
+            <a href={project.deployLink} className="project-button" target="_blank" rel="noopener noreferrer">
+              Deploy
+            </a>
+          )}
+          <a href={project.repoLink} className="project-button" target="_blank" rel="noopener noreferrer">
+            Repository
           </a>
         </div>
       ))}
